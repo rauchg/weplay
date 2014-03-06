9a6afe8b513b7389fe787c94fe0261b7721cd65c
Guillermo Rauch <rauchg@gmail.com>
2014-03-05 18:14:39 -0800

    remove web stuff, moving into own repo

--- Makefile
+++
@@ -1,3 +0,0 @@
-
-public/main.js: package.json client/*
-	@./node_modules/.bin/browserify client/app.js -o public/main.js
--- index.js
+++ index.js
@@ -1,51 +1,16 @@
 
-var http = require('http').Server;
 var sio = require('socket.io');
 var browserify = require('browserify-middleware');
-var mustache = require('mustache-express');
-var express = require('express');
 var request = require('superagent');
-var app = express();
 
-var srv = http(app);
-var io = module.exports = sio(srv);
 var port = process.env.WEPLAY_PORT || 3000;
+var io = module.exports = sio(port);
+console.log('listening on *:' + port);
 
 var redis = require('./redis')();
 var pubsub = require('./redis')();
 
 process.title = 'weplay-io';
-
-srv.listen(port);
-console.log('listening on *:' + port);
-
-if ('development' != process.env.NODE_ENV) {
-  app.use(express.basicAuth('a', 'b'));
-}
-
-app.engine('mustache', mustache());
-app.set('views', __dirname + '/views');
-
-if ('development' == process.env.NODE_ENV) {
-  app.use('/main.js', browserify('./client/app.js'));
-}
-app.use(express.static(__dirname + '/public'));
-
-app.use(function(req, res, next){
-  req.socket.on('error', function(err){
-    console.error(err.stack);
-  });
-  next();
-});
-
-app.get('/', function(req, res, next){
-  redis.get('weplay:frame', function(err, image){
-    if (err) return next(err);
-    res.render('index.mustache', {
-      img: image.toString('base64')
-    });
-  });
-});
 
 var keys = {
   right: 0,
